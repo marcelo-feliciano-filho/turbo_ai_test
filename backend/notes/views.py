5ce0c048887583dc4c5a4f12f3ae52f419c94909
Marcelo Feliciano Filho <marcelo.feliciano@salessense.com.br>
2025-02-13 19:51:37 -0300

    Backend authentication and notes API implemented

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Note
+from .serializers import NoteSerializer
 
-# Create your views here.
+
+class NoteViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Note.objects.all()
+
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user)  # Restrict to logged-in user's notes
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
